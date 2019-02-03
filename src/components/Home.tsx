f7cb23a9cb6a3d0e92fb8b6f8730ba3b7c68a89f
Mehrad Rousta <mehrad77@gmail.com>
2019-02-03 13:09:51 +0330

    remove postCSS and it files

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,11 @@
-import * as React from 'react'
-import * as classnames from 'classnames'
-import * as css from './Home.css'
+import * as React from 'react';
+import * as classnames from 'classnames';
 
-export const Home: React.FunctionComponent = props =>
-  <div className={classnames('test', css.home)}>
+export const Home: React.FunctionComponent = props => (
+  <div>
     <ul>
-      <li>
-        usage classnames in Home.tsx
-      </li>
-      <li>
-        Layout.tsx set background-color hot-pink using global styled jsx
-      </li>
+      <li>usage classnames in Home.tsx</li>
+      <li>Layout.tsx set background-color hot-pink using global styled jsx</li>
     </ul>
   </div>
+);
