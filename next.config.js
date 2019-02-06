159aa2f069b7716b04fec751e969f5175ce68b44
Mehrad Rousta <mehrad77@gmail.com>
2019-02-06 13:54:32 +0330

    add image and tslint plugins

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
-const withTypescript = require('@zeit/next-typescript');
-// const withCSS                = require('@zeit/next-css')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const withPlugins = require('next-compose-plugins');
+const images = require('next-images');
+const typescript = require('@zeit/next-typescript');
 
-module.exports = withTypescript();
-// withCSS({
-// webpack(config) {
-// if (process.env.ANALYZE) {
-//   config.plugins.push(new BundleAnalyzerPlugin({
-//     analyzerMode: 'server',
-//     analyzerPort: 8888,
-//     openAnalyzer: true
-//   }))
-// }
-// return config
-// }
-// ,
-// cssModules: true,
-// })
+// next.js configuration
+const nextConfig = {
+  // useFileSystemPublicRoutes: false,
+  // distDir: 'build',
+};
+
+module.exports = withPlugins([images, typescript], nextConfig);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react'
-import {Nav} from './Nav'
+import * as React from 'react';
+import { Nav } from './Nav';
+import * as Logo from '../../static/logo.svg';
 
-export const Header: React.FunctionComponent = props =>
+export const Header: React.FunctionComponent = props => (
   <div>
-    <h2>
-      header
-    </h2>
-    <Nav/>
+    <h2>header</h2>
+    <img src={Logo} />
+    <Nav />
   </div>
+);
