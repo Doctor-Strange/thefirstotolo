d521892d1cff11ebc3c5c727e3c1b14f80f36ed9
Mehrad Rousta <mehrad77@gmail.com>
2019-02-10 11:34:48 +0330

    add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Link from 'next/link';
+import * as LogoSVG from '../../static/logo_sticky.svg';
+
+export const Logo: React.FunctionComponent = props => (
+  <div id="logo">
+    <a href="/">
+      <img
+        src={LogoSVG}
+        width="165"
+        height="35"
+        alt=""
+        className="logo_sticky"
+      />
+    </a>
+  </div>
+);
