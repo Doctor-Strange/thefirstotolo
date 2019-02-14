32a15febed08169c6f9636de7c64aa951671bdd5
Mehrad Rousta <mehrad77@gmail.com>
2019-02-14 12:29:05 +0330

    fix LoginModal Styles

--- src/components/Modals/LoginModal.tsx
+++ src/components/Modals/LoginModal.tsx
@@ -1,7 +1,160 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import ReactModal from 'react-modal';
 import { ModalWrapper } from './ModalWrapper';
+
+const LoginForm = styled.form`
+  .form-group {
+    position: relative;
+
+    input.form-control {
+      padding-left: 40px;
+    }
+
+    i {
+      font-size: 21px;
+      font-size: 1.3125rem;
+      position: absolute;
+      left: 12px;
+      top: 25px;
+      color: #ccc;
+      width: 25px;
+      height: 25px;
+      display: block;
+      font-weight: 400 !important;
+    }
+
+    .my-toggle {
+      background: transparent;
+      border: 0;
+      -webkit-border-radius: 3px;
+      -moz-border-radius: 3px;
+      -ms-border-radius: 3px;
+      border-radius: 3px;
+      color: #888;
+      cursor: pointer;
+      font-size: 10px;
+      font-size: 10px;
+      font-size: 0.625rem;
+      font-weight: bold;
+      margin-right: 5px;
+      height: 30px;
+      line-height: 30px;
+      padding: 0 10px;
+      text-transform: uppercase;
+      -moz-appearance: none;
+      -webkit-appearance: none;
+      background-color: #fff;
+      :hover,
+      :focus {
+        background-color: #eee;
+        color: #555;
+        outline: transparent;
+      }
+    }
+  }
+
+  .hideShowPassword-wrapper {
+    width: 100% !important;
+  }
+
+  .checkboxes label {
+    color: #999;
+    margin-top: 5px;
+  }
+
+  a#forgot {
+    color: #999;
+    font-weight: 500;
+    font-size: 13px;
+    font-size: 0.8125rem;
+  }
+
+  #forgot_pw {
+    background-color: #fff;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    z-index: 99;
+    min-height: 430px;
+    display: none;
+    padding: 25px;
+
+    @media (max-width: 767px) {
+      padding: 0;
+    }
+
+    label {
+      font-weight: 500;
+    }
+  }
+
+  .sign-in-wrapper {
+    position: relative;
+    height: 100%;
+  }
+
+  a.social_bt {
+    border-radius: 3px;
+    text-align: center;
+    color: #fff;
+    min-width: 200px;
+    margin-bottom: 15px;
+    display: block;
+    padding: 12px;
+    line-height: 1;
+    position: relative;
+    transition: all 0.3s ease-in-out;
+    cursor: pointer;
+    :hover {
+      filter: brightness(115%);
+    }
+  }
+
+  a.social_bt.facebook,
+  a.social_bt.google,
+  a.social_bt.linkedin {
+    :before {
+      position: absolute;
+      left: 12px;
+      top: 10px;
+      font-size: 1rem;
+      color: #fff;
+    }
+  }
+
+  a.social_bt.facebook {
+    background-color: #3b5998;
+    :before {
+      content: 'f';
+    }
+  }
+
+  a.social_bt.google {
+    background-color: #dc4e41;
+    :before {
+      content: 'g';
+    }
+  }
+
+  .divider {
+    text-align: center;
+    height: 1px;
+    margin: 30px 0 20px 0;
+    background-color: #e1e8ed;
+    span {
+      position: relative;
+      top: -20px;
+      background-color: #fff;
+      display: inline-block;
+      padding: 10px;
+      font-style: italic;
+    }
+  }
+  .flow-root {
+    display: flow-root;
+  }
+`;
 
 export class LoginModal extends React.Component<{ onRef: any }> {
   [x: string]: any;
@@ -18,8 +171,85 @@ export class LoginModal extends React.Component<{ onRef: any }> {
 
   render() {
     return (
+      // tslint:disable-next-line:jsx-no-lambda
       <ModalWrapper title="Sign In" onRef={ref => (this.modalwrapper = ref)}>
-        Test
+        <LoginForm>
+          <div className="sign-in-wrapper">
+            <a href="#0" className="social_bt facebook">
+              Login with Facebook
+            </a>
+            <a href="#0" className="social_bt google">
+              Login with Google
+            </a>
+            <div className="divider">
+              <span>Or</span>
+            </div>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                id="email"
+              />
+              <i className="icon_mail_alt"> X </i>
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="password"
+                value=""
+              />
+              <i className="icon_lock_alt"> X </i>
+            </div>
+            <div className="clearfix add_bottom_15 flow-root">
+              <div className="checkboxes float-left">
+                <label className="container_check">
+                  Remember me
+                  <input type="checkbox" />
+                  <span className="checkmark" />
+                </label>
+              </div>
+              <div className="float-right mt-1">
+                <a id="forgot" href="javascript:void(0);">
+                  Forgot Password?
+                </a>
+              </div>
+            </div>
+            <div className="text-center">
+              <input
+                type="submit"
+                value="Log In"
+                className="btn_1 full-width"
+              />
+            </div>
+            <div className="text-center">
+              Don’t have an account? <a href="/register">Sign up</a>
+            </div>
+            <div id="forgot_pw">
+              <div className="form-group">
+                <label>Please confirm login email below</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email_forgot"
+                  id="email_forgot"
+                />
+                <i className="icon_mail_alt" />
+              </div>
+              <p>
+                You will receive an email containing a link allowing you to
+                reset your password to a new preferred one.
+              </p>
+              <div className="text-center">
+                <input type="submit" value="Reset Password" className="btn_1" />
+              </div>
+            </div>
+          </div>
+        </LoginForm>
       </ModalWrapper>
     );
   }
--- src/theme/globalStyle.tsx
+++ src/theme/globalStyle.tsx
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700');
   *, ::after, ::before {
     box-sizing: border-box;
   }
@@ -49,7 +50,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
 
-  /** bootstrap styles **/
+  /**============================== Bootstrap Styles ==============================**/
   .container {
     width: 100%;
     padding-right: 15px;
@@ -75,7 +76,7 @@ const GlobalStyle = createGlobalStyle`
     margin-right: -15px;
     margin-left: -15px;
   }
-  /** to remove **/
+
   .col-12 {
     flex: 0 0 100%;
     max-width: 100%;
@@ -100,6 +101,17 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 
+  .mt-1 {
+      margin-top: .25rem!important;
+  }
+
+  .float-right {
+      float: right!important;
+  }
+
+  .float-left {
+      float: left!important;
+  }
 
   .form-control {
     display: block;
@@ -120,6 +132,11 @@ const GlobalStyle = createGlobalStyle`
     margin-bottom: 1rem;
   }
   
+  .text-center {
+      text-align: center!important;
+  }
+
+  /*============================== Modal Styles ==============================*/
   .md-modal {
     position: fixed;
     top: 50%;
@@ -159,6 +176,357 @@ const GlobalStyle = createGlobalStyle`
   .md-show.md-overlay {
     background: hsla(0, 0%, 0%, 0.8);
   }
+
+  /*============================== btn Styles ==============================*/
+  .btn_1 {
+    border: none;
+    color: #fff;
+    background: #004dda;
+    outline: none;
+    cursor: pointer;
+    display: inline-block;
+    text-decoration: none;
+    padding: 15px 30px;
+    color: #fff;
+    font-weight: 600;
+    text-align: center;
+    line-height: 1;
+    transition: all 0.3s ease-in-out;
+    border-radius: 3px;
+
+    :hover {
+      background-color: #FFC107;
+      color: #222 !important;
+    }
+
+    &.full-width {
+      display: block;
+      width: 100%;
+      text-align: center;
+      margin-bottom: 5px;
+
+      .purchase {
+        background-color: #004dda;
+        :hover {
+          background-color: #32a067;
+          color: #fff !important;
+        }
+      }
+      
+      .wishlist {
+        border-color: #555;
+        color: #555;
+        :hover {
+          color: #fff !important;
+          background-color: #555;
+          border-color: #555;
+        }
+      }
+
+    }
+
+    .medium {
+      font-size: 16px;
+      font-size: 1rem;
+      padding: 18px 40px;
+    }
+
+    .rounded {
+      border-radius: 25px !important;
+    }
+
+    .outline {
+      border: 2px solid #004dda;
+      color: #004dda;
+      padding: 11px 40px;
+      background: none;
+
+      :hover {
+        background: #0054a6;
+        color: #fff;
+        border-color: #0054a6;
+      }
+
+      :focus {
+        outline: none;
+      }
+    }
+  }
+
+  /**============================== Forms Style ==============================**/
+.form-control {
+    height: calc(2.55rem + 2px);
+    font-size: 14px;
+    font-size: 0.875rem;
+    border-radius: 3px;
+    border: 1px solid #d2d8dd;
+}
+
+/* Checkbox style */
+.container_check {
+  display: block;
+  position: relative;
+  font-size: 14px;
+  font-size: 0.875rem;
+  padding-left: 30px;
+  line-height: 1.4;
+  margin-bottom: 10px;
+  cursor: pointer;
+  user-select: none;
+  input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+  }
+  input:checked ~ .checkmark {
+    background-color: #004dda;
+    border: 1px solid transparent;
+  }
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 20px;
+    width: 20px;
+    border: 1px solid #d2d8dd;
+    border-radius: 3px;
+    transition: all 0.3s ease-in-out;
+    :after {
+      content: "";
+      position: absolute;
+      display: none;
+      left: 7px;
+      top: 3px;
+      width: 5px;
+      height: 10px;
+      border: solid white;
+      border-width: 0 2px 2px 0;
+      -webkit-transform: rotate(45deg);
+      -ms-transform: rotate(45deg);
+      transform: rotate(45deg);
+    }
+  }
+  input:checked ~ .checkmark:after {
+    display: block;
+  }
+}
+
+/* Radio buttons */
+.container_radio {
+  display: block;
+  position: relative;
+  font-size: 15px;
+  font-size: 0.9375rem;
+  padding-left: 30px;
+  line-height: 1.3;
+  margin-bottom: 10px;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  input {
+    position: absolute;
+    opacity: 0;
+  }
+  input:checked ~ .checkmark:after {
+    opacity: 1;
+  }
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 20px;
+    width: 20px;
+    border: 1px solid #ccc;
+    border-radius: 50%;
+    :after {
+      display: block;
+      content: "";
+      position: absolute;
+      opacity: 0;
+      transition: all 0.3s ease-in-out;
+      top: 3px;
+      left: 3px;
+      width: 12px;
+      height: 12px;
+      border-radius: 50%;
+      background: #004dda;
+      transition: all 0.3s ease-in-out;
+    }
+  }
+}
+
+/**============================== 3.4 Spacing ==============================*/
+.add_bottom_10 {
+  margin-bottom: 10px;
+}
+
+.add_bottom_15 {
+  margin-bottom: 15px;
+}
+
+.add_bottom_30 {
+  margin-bottom: 30px;
+}
+
+.add_bottom_45 {
+  margin-bottom: 45px;
+}
+
+.add_bottom_60 {
+  margin-bottom: 60px;
+}
+
+.add_bottom_75 {
+  margin-bottom: 75px;
+}
+
+.add_top_10 {
+  margin-top: 10px;
+}
+
+.add_top_15 {
+  margin-top: 15px;
+}
+
+.add_top_20 {
+  margin-top: 20px;
+}
+
+.add_top_30 {
+  margin-top: 30px;
+}
+
+.add_top_60 {
+  margin-top: 60px;
+}
+
+.more_padding_left {
+  padding-left: 40px;
+}
+
+.nomargin_top {
+  margin-top: 0;
+}
+
+.nopadding {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+
+.nomargin {
+  margin: 0 !important;
+}
+
+.margin_30 {
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+
+.margin_30_5 {
+  padding-top: 30px;
+  padding-bottom: 5px;
+}
+
+.margin_60 {
+  padding-top: 60px;
+  padding-bottom: 60px;
+}
+@media (max-width: 767px) {
+  .margin_60 {
+    padding-top: 30px;
+    padding-bottom: 30px;
+  }
+}
+
+.margin_60_35 {
+  padding-top: 60px;
+  padding-bottom: 35px;
+}
+@media (max-width: 575px) {
+  .margin_60_35 {
+    padding-top: 30px;
+    padding-bottom: 5px;
+  }
+}
+
+.margin_80 {
+  padding-top: 80px;
+  padding-bottom: 80px;
+}
+@media (max-width: 991px) {
+  .margin_80 {
+    padding-bottom: 60px;
+    padding-top: 60px;
+  }
+}
+@media (max-width: 575px) {
+  .margin_80 {
+    padding-top: 30px;
+    padding-bottom: 30px;
+  }
+}
+
+.margin_80_55 {
+  padding-top: 80px;
+  padding-bottom: 55px;
+}
+@media (max-width: 991px) {
+  .margin_80_55 {
+    padding-top: 60px;
+    padding-bottom: 35px;
+  }
+}
+@media (max-width: 575px) {
+  .margin_80_55 {
+    padding-top: 45px;
+    padding-bottom: 15px;
+  }
+}
+
+.margin_80_0 {
+  padding-top: 80px;
+}
+@media (max-width: 991px) {
+  .margin_80_0 {
+    padding-top: 60px;
+  }
+}
+@media (max-width: 575px) {
+  .margin_80_0 {
+    padding-top: 30px;
+  }
+}
+
+.margin_30_95 {
+  padding-top: 30px;
+  padding-bottom: 95px;
+}
+@media (max-width: 991px) {
+  .margin_30_95 {
+    padding-top: 15px;
+    padding-bottom: 35px;
+  }
+}
+@media (max-width: 575px) {
+  .margin_30_95 {
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+}
+
+@media (max-width: 991px) {
+  .hidden_tablet {
+    display: none !important;
+  }
+}
+
+@media (max-width: 767px) {
+  .hidden_mobile {
+    display: none !important;
+  }
+}
 `;
 
 const lightTheme = {
