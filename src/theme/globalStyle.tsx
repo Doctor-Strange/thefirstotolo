02ffd47c6b364eb9af3762dab7ba0dc6820f4836
Mehrad Rousta <mehrad77@gmail.com>
2019-02-19 12:44:37 +0330

    add more info to login form

--- src/theme/globalStyle.tsx
+++ src/theme/globalStyle.tsx
@@ -560,6 +560,28 @@ const GlobalStyle = createGlobalStyle`
     display: none !important;
   }
 }
+
+
+
+
+
+
+
+
+.react-phone-number-input__country-select{
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    user-select: none;
+    border: 1px solid transparent;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: .25rem;
+    width: 70px;
+}
 `;
 
 const lightTheme = {
