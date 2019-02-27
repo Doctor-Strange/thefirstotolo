984c6c4568f95bd8760ecab23a3313b35b24e3f0
Mehrad Rousta <mehrad77@gmail.com>
2019-02-27 13:38:26 +0330

    add Formik configs and Yup

--- src/components/Modals/LoginModal.tsx
+++ src/components/Modals/LoginModal.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { Select } from 'semantic-ui-react';
-import { Formik } from 'formik';
+import { Formik, FormikActions } from 'formik';
+import * as Yup from 'yup';
 import { LoginForm, PhoneRow } from './LoginStyle';
 import { ModalWrapper } from './ModalWrapper';
 import { PanelsWrapper } from '../Carousel/PanelsWrapper';
 import { Panel } from '../Carousel/Panel';
 import { mobileNumberOptions } from '../../constants/options';
+
+interface LoginModalValues {
+  phone: number;
+}
 
 export class LoginModal extends React.Component<
   { onRef: any },
@@ -33,14 +38,6 @@ export class LoginModal extends React.Component<
     this.props.onRef(undefined);
   }
 
-  handleSubmit(e) {
-    // tslint:disable-next-line:no-console
-    console.log('handleSubmit handled!');
-  }
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
   nextPanel() {
     if (this.state.showIndex + 1 > 3) return;
     this.setState({
@@ -66,84 +63,111 @@ export class LoginModal extends React.Component<
           prevIndex={this.state.prevIndex}
         >
           <Panel>
-            <LoginForm>
-              <div className="sign-in-wrapper">
-                <div className="form-group">
-                  <label>Phone Number</label>
-                  <PhoneRow>
-                    <input
-                      type="number"
-                      className="form-control input"
-                      name="phone"
-                      id="phone"
-                      onChange={this.handleChange}
-                      value={this.state.phone}
-                    />
-                    <Select
-                      name="country"
-                      id="country"
-                      className=" form-control country"
-                      placeholder="+98"
-                      options={mobileNumberOptions}
-                    />
-                  </PhoneRow>
-                </div>
-                <div className="clearfix add_bottom_15 flow-root">
-                  <div className="checkboxes float-left">
-                    <label className="container_check">
-                      Remember me
-                      <input type="checkbox" />
-                      <span className="checkmark" />
-                    </label>
-                  </div>
-                  <div className="float-right mt-1">
-                    <a id="forgot" href="javascript:void(0);">
-                      Forgot Password?
+            <Formik
+              initialValues={{ phone: null }}
+              onSubmit={(
+                values: LoginModalValues,
+                actions: FormikActions<LoginModalValues>
+              ) => {
+                // tslint:disable-next-line: jsx-no-lambda
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  actions.setSubmitting(false);
+                }, 400);
+              }}
+              validationSchema={Yup.object().shape({
+                phone: Yup.number().required('Required')
+              })}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                isSubmitting,
+                values,
+                errors,
+                touched
+              }) => (
+                <LoginForm onSubmit={handleSubmit}>
+                  <div className="sign-in-wrapper">
+                    <div className="form-group">
+                      <label>Phone Number</label>
+                      <PhoneRow>
+                        <input
+                          type="number"
+                          className="form-control input"
+                          name="phone"
+                          id="phone"
+                          onChange={handleChange}
+                          value={values.phone}
+                        />
+                        <Select
+                          name="country"
+                          id="country"
+                          className=" form-control country"
+                          placeholder="+98"
+                          options={mobileNumberOptions}
+                        />
+                      </PhoneRow>
+                      {errors.phone && touched.phone && errors.phone}
+                    </div>
+                    <div className="clearfix add_bottom_15 flow-root">
+                      <div className="checkboxes float-left">
+                        <label className="container_check">
+                          Remember me
+                          <input type="checkbox" />
+                          <span className="checkmark" />
+                        </label>
+                      </div>
+                      <div className="float-right mt-1">
+                        <a id="forgot" href="javascript:void(0);">
+                          Forgot Password?
+                        </a>
+                      </div>
+                    </div>
+                    <div className="text-center">
+                      <input
+                        type="submit"
+                        value="Log In"
+                        className="btn_1 full-width"
+                        disabled={isSubmitting}
+                      />
+                    </div>
+                    <div className="text-center">
+                      Don’t have an account? <a href="/register">Sign up</a>
+                    </div>
+                    <div id="forgot_pw">
+                      <div className="form-group">
+                        <label>Please confirm login email below</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          name="email_forgot"
+                          id="email_forgot"
+                        />
+                        <i className="icon_mail_alt" />
+                      </div>
+                      <p>
+                        You will receive an email containing a link allowing you
+                        to reset your password to a new preferred one.
+                      </p>
+                      <div className="text-center">
+                        <input
+                          type="submit"
+                          value="Reset Password"
+                          className="btn_1"
+                        />
+                      </div>
+                    </div>
+                    <div className="divider">
+                      <span>Or</span>
+                    </div>
+                    <a href="#0" className="social_bt google">
+                      Login with Google
                     </a>
                   </div>
-                </div>
-                <div className="text-center">
-                  <input
-                    type="submit"
-                    value="Log In"
-                    className="btn_1 full-width"
-                    onClick={this.nextPanel}
-                  />
-                </div>
-                <div className="text-center">
-                  Don’t have an account? <a href="/register">Sign up</a>
-                </div>
-                <div id="forgot_pw">
-                  <div className="form-group">
-                    <label>Please confirm login email below</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      name="email_forgot"
-                      id="email_forgot"
-                    />
-                    <i className="icon_mail_alt" />
-                  </div>
-                  <p>
-                    You will receive an email containing a link allowing you to
-                    reset your password to a new preferred one.
-                  </p>
-                  <div className="text-center">
-                    <input
-                      type="submit"
-                      value="Reset Password"
-                      className="btn_1"
-                    />
-                  </div>
-                </div>
-                <div className="divider">
-                  <span>Or</span>
-                </div>
-                <a href="#0" className="social_bt google">
-                  Login with Google
-                </a>
-              </div>
-            </LoginForm>
+                </LoginForm>
+              )}
+            </Formik>
           </Panel>
           <Panel>
             <LoginForm>
