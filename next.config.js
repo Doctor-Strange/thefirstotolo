67f3710575789a01b7148b61ce374a0d3721b5a3
Mehrad Rousta <mehrad77@gmail.com>
2019-03-05 17:14:55 +0330

    add zeit/next-css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withPlugins = require('next-compose-plugins');
 const images = require('next-images');
 const typescript = require('@zeit/next-typescript');
+const CSS = require('@zeit/next-css');
+
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = () => {};
+  require.extensions['.css'] = file => {};
+}
 
 // next.js configuration
 const nextConfig = {
@@ -8,4 +14,4 @@ const nextConfig = {
   // distDir: 'build',
 };
 
-module.exports = withPlugins([images, typescript], nextConfig);
+module.exports = withPlugins([images, typescript, CSS], nextConfig);
