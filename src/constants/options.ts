3f55129009aa8508b8b21bd07e858d2a0d2cb6d2
Mehrad Rousta <mehrad77@gmail.com>
2019-03-06 14:16:11 +0330

    add monthsEnglish to options const

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -5,3 +5,18 @@ export const countryOptions = [
 export const mobileNumberOptions = [
   { key: 'ir', value: 'ir', flag: 'ir', text: '+98' }
 ];
+
+export const monthsEnglish = [
+  { key: '1', value: '1', text: 'Farvardin' },
+  { key: '2', value: '2', text: 'Ordibehesht' },
+  { key: '3', value: '3', text: 'Khordad' },
+  { key: '4', value: '4', text: 'Tir' },
+  { key: '5', value: '5', text: 'Mordad' },
+  { key: '6', value: '6', text: 'Shahrivar' },
+  { key: '7', value: '7', text: 'Mehr' },
+  { key: '8', value: '8', text: 'Aban' },
+  { key: '9', value: '9', text: 'Azar' },
+  { key: '10', value: '10', text: 'Dey' },
+  { key: '11', value: '11', text: 'Bahman' },
+  { key: '12', value: '12', text: 'Esfand' }
+];
