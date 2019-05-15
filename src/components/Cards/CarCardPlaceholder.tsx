5828c48b9292e86414a6a0a35c9d3b130a9b3ff1
Mehrad Rousta <mehrad77@gmail.com>
2019-05-15 11:32:08 +0430

    update car place holder

--- src/components/Cards/CarCardPlaceholder.tsx
+++ src/components/Cards/CarCardPlaceholder.tsx
@@ -1,26 +1,18 @@
 
 import ContentLoader from "react-content-loader"
 export const CarCardPlaceholder = () => (
-  <ContentLoader 
+  <ContentLoader
     height={300}
     width={300}
-    speed={1}
+    speed={4}
     rtl
-    primaryColor="#d2d2d2"
-    secondaryColor="#ecebeb"
-    style={{width: '300px', height: '300px', margin: '10px auto'}}
+    primaryColor="#d2d2d240"
+    secondaryColor="#ecebeb40"
+    style={{ width: '300px', height: '300px', margin: '10px auto' }}
     className="carcard"
   >
-    <rect x="10" y="10" rx="3" ry="3" width="263" height="179" /> 
-    <rect x="20" y="230" rx="3" ry="3" width="70" height="10" /> 
-    <rect x="100" y="230" rx="3" ry="3" width="100" height="10" /> 
-    <rect x="210" y="230" rx="3" ry="3" width="10" height="10" /> 
-    <rect x="35" y="250" rx="3" ry="3" width="130" height="10" /> 
-    <rect x="175" y="250" rx="3" ry="3" width="80" height="10" /> 
-    <rect x="35" y="270" rx="3" ry="3" width="90" height="10" /> 
-    <rect x="135" y="270" rx="3" ry="3" width="60" height="10" /> 
-    <rect x="205" y="270" rx="3" ry="3" width="60" height="10" /> 
-    <rect x="20" y="290" rx="3" ry="3" width="30" height="10" /> 
-    <rect x="20" y="200" rx="3" ry="3" width="208" height="20" />
+    <rect x="5" y="10" rx="3" ry="3" width="350" height="195" />
+    <rect x="5" y="215" rx="3" ry="3" width="220" height="20" />
+    <rect x="5" y="240" rx="3" ry="3" width="80" height="15" />
   </ContentLoader>
 )
