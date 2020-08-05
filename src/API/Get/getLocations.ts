0b8d5f751dacdf3eabd498ec93c6814c8f73591e
sajad saderi <saderi.sajad@gmail.com>
2020-08-05 09:46:44 +0430

    trest

--- src/API/Get/getLocations.ts
+++ src/API/Get/getLocations.ts
@@ -6,7 +6,7 @@ const GET_LOCATIONS = '/core/location/list';
 export const REQUEST_getLocations = (data: IgetLocation) => {
   return new Promise((resolve, reject) => {
     axios
-      .post(DOMAIN + GET_LOCATIONS + '?brief=' + data.brief)
+      .post(DOMAIN + GET_LOCATIONS + '?limit=900&brief=' + data.brief)
       .then(response => {
         if (response.data.success) {
           const citiesFarsi = response.data.items.map((value, index) => ({
